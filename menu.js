e968af2c2319089c7711bb7b6ad23268824d3785
gerpan <nhaths4701@gmail.com>
2021-02-04 16:03:25 +0700

    fix responsive menu

--- menu.js
+++ menu.js
@@ -1,26 +1,26 @@
 const body = document.body
+const mobileMenuTrigger = document.getElementById('mobile-menu-trigger')
+const desktopMenu = document.getElementById('desktop-menu')
+const mobileMenu = document.getElementById('mobile-menu')
+
+mobileMenuTrigger.addEventListener('click', () => {
+    if (mobileMenu.style.display === 'none') {
+        mobileMenu.style.display = 'block'
+    } else {
+        mobileMenu.style.display = 'none'
+    }
+})
 
 const responsiveMenu = () => {
-    const mobileMenuTrigger = document.getElementById('mobile-menu-trigger')
-    const desktopMenu = document.getElementById('desktop-menu')
-    const mobileMenu = document.getElementById('mobile-menu')
-
-    mobileMenuTrigger.addEventListener('click', () => {
-        if (mobileMenu.style.display === 'none') {
-            mobileMenu.style.display = 'block'
-        } else {
-            mobileMenu.style.display = 'none'
-        }
-    })
-
     if (window.innerWidth <= 991) {
         desktopMenu.style.display = 'none'
         mobileMenuTrigger.style.display = 'block'
     } else {
         desktopMenu.style.display = 'block'
         mobileMenuTrigger.style.display = 'none'
+        mobileMenu.style.display = 'none'
     }
 }
 
 responsiveMenu()
-body.onresize = function () {responsiveMenu()}
+body.onresize = () => responsiveMenu()
